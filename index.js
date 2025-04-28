4980e7d1ac3fbe5a5795582e18e17068c6510878
jayrard17 <jayrard17@gmail.com>
2025-04-28 16:42:32 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    $("header nav a").on("click", function (e) {
+        e.preventDefault();
+
+        const target = $(this).attr("href");
+        let offset = 0;
+
+        if (target !== "#home") {
+            offset = $(target).offset().top - $("#navbar").outerHeight();
+        }
+
+        $("html, body").animate({
+            scrollTop: offset
+        }, 100);
+    });
+});
+$(document).ready(function () {
+    // Open sidenav
+    $('#menu-toggle').click(function () {
+        $('#mySidenav').removeClass('translate-x-full').addClass('translate-x-0');
+    });
+
+    // Close sidenav
+    $('#closebtn').click(function () {
+        $('#mySidenav').removeClass('translate-x-0').addClass('translate-x-full');
+    });
+
+    // Close sidenav on nav link click
+    $('#mySidenav a').click(function () {
+        $('#mySidenav').removeClass('translate-x-0').addClass('translate-x-full');
+    });
+});
+const toggleBtn = document.getElementById('menu-toggle');
+const mobileMenu = document.getElementById('mobile-menu');
+
+toggleBtn.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+});
